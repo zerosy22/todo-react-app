e982fe454de4a1e240342aa9d872e40fe2e003b1
zerosy <zerosy22@gmail.com>
2024-01-30 17:41:26 +0900

    event handling

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -5,7 +5,40 @@ import {TextField, Paper, Button, Grid} from "@material-ui/core"
 class AddToDo extends React.Component{
     constructor(props){
         super(props);
+
+        // App.js 파일에서 넘겨준 메서드를 add 에 저장
+        this.add = props.add;
+
+        // 입력한 문자열을 저장하기 위한 속성을 생성
+        this.state = {item:{title:""}}
     }
+
+    // TextField에서 값을 입력할 때 마다 호출되는 이벤트 리스너
+    // 이벤트 처리하는 함수의 모양은 무조건 이와 같다. e 는 매개변수
+    onInputChange = (e) => {
+        // state.title 에 입력한 내용을 대입
+        const thisItem = this.state.item
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
+        // 내용이 바뀌는 걸 console로 확인
+        console.log(this.state.item.title)
+    }
+
+    // + 버튼을 눌렀을 때 호출되는 이벤트 리스너
+    onButtonClick = (e) => {
+        // 배열에 데이터를 삽입하는 함수를 호출
+        this.add(this.state.item);
+        // 텍스트 필드를 초기화
+        this.setState({item:{title:""}})
+    }
+
+    // Enter를 누르면 호출되는 이벤트 리스너
+    // 우리나라 사이트에서 특히 많이 만들었음
+    enterKeyEventHandler = (e) => {
+        if(e.key === "Enter"){
+            this.onButtonClick();
+        }
+    }// 트리거 : 나에게 이벤트가 발생했는데 다른 이벤트를 리스너를 불러오는 것 DB에서 많이 사용
 
 
     render(){
@@ -13,10 +46,12 @@ class AddToDo extends React.Component{
             <Paper style={{margin:16, padding:16}}>
                 <Grid container>   {/* 옆으로 */}
                     <Grid xs={11} md={11} item style={{paddingRight:16}}> {/* 기본 default 12 */}
-                        <TextField placeholder="여기에 작성하시오!!!" fullWidth />
+                        <TextField placeholder="여기에 작성하시오!!!" fullWidth 
+                        onChange={this.onInputChange} value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHandler} />
                     </Grid> {/* 남은 1에 버튼을 만들고 싶어서 */}
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary">
+                        <Button fullWidth color="secondary" onClick={this.onButtonClick}>
                             +
                         </Button>
                     </Grid> 
--- src/App.js
+++ src/App.js
@@ -26,6 +26,28 @@ class App extends React.Component {
                  }
   }
 
+  // 데이터를 추가하는 함수
+  add = (item) => {
+    // react는 props는 수정할 수 없고
+    // state는 수정이 가능하지만 원본을 직접 수정하는 것은 안되고
+    // setState 메서드를 이용해서만 수정이 가능합니다.
+    // react는 바뀐곳을 원본과 비교해서 바뀐곳을 출력해주기 때문에
+    // 바뀐곳이 나중에 다시 원본으로 들어감
+    // 관계형 데이터 베이스 와 원리가 같다. commit 과 rollback 이 이와 같음
+
+    // 배열에 데이터를 추가하기 위해서 state의 배열을 복사
+    const thisItems = this.state.items;
+
+    // item의 id와 done 값을 설정 - title만 입력하기 때문
+    item.id = "ID_" + thisItems.length;
+    item.done = false;
+
+    // 복사한 데이터에 데이터를 추가
+    thisItems.push(item)
+    // 복사한 데이터를 다시 state에 적용
+    this.setState({items:thisItems});
+  }
+
   render(){
     // 배열을 순회하면서 출력할 내용을 생성
     // item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
@@ -50,7 +72,7 @@ class App extends React.Component {
     return(
       <div className="App">
         <Container maxWidth="md">
-          <AddToDo />
+          <AddToDo add={this.add}/>   {/* add메서드를 AddToDo에 넘겨줌 */}
           {display}
         </Container>
       </div>
